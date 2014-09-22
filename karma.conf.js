f8fd41a8ca6a428569195a3f3a7c7878957e8637
Farruco Sanjurjo <madtrick@gmail.com>
2014-09-22 20:28:31 +0200

    Setup testing environment

---
+++ karma.conf.js
@@ -0,0 +1,37 @@
+module.exports = function(config) {
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine', 'browserify'],
+
+        // list of files / patterns to load in the browser
+        files: [
+          'app/**/__tests__/*.js'
+        ],
+
+        // list of files to exclude
+        exclude: [],
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+          'app/**/__tests__/*.js': ['browserify']
+        },
+        browserify: {
+            debug: true,
+            transform: [ 'reactify' ]
+        },
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false
+    });
+};
+
