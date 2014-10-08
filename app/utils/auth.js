a89787b7d526d7059935740e226985d7f5b9e87b
Farruco Sanjurjo <madtrick@gmail.com>
2014-10-08 21:12:17 +0200

    Replace hardcoded client id with variable

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -3,12 +3,12 @@ var params   = require('./params');
 var hello = require('hellojs');
 
 
-hello.init({google:  '147125267354-eg1dootuvr0odr0blj9q5ge66adqtohl.apps.googleusercontent.com'});
 
 var Auth = redefine.Class({
   statics: {
     start: function(clientId, redirectUri, callback){
       if (!params().token) {
+        hello.init({google:  clientId});
           hello.login('google', {redirect_uri: redirectUri, response_type: 'code', display: 'page', scope: ['https://www.googleapis.com/auth/userinfo.email']});
       } else
         callback();
