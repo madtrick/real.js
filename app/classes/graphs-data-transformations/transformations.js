142f38dfdbe3972f137bf65404c8671042b16d7f
Farruco Sanjurjo <madtrick@gmail.com>
2014-12-08 19:45:07 +0100

    Return a new object instead of modifying it in place

--- app/classes/graphs-data-transformations/transformations.js
+++ app/classes/graphs-data-transformations/transformations.js
@@ -4,9 +4,9 @@ var _        = require('lodash');
 var Transformations = redefine.Class({
   accumulatedPerDay: function(data){
     var acc = 0;
-    return _.each(data, function(obj){
+    return _.map(data, function(obj){
       acc = obj.value + acc;
-      obj.value = acc;
+      return _.extend({}, obj, {value: acc});
     });
   }
 });
