c5b6c45c4803dc835c0bf5a69773c4cadbc82f88
Farruco Sanjurjo <madtrick@gmail.com>
2014-12-09 19:56:37 +0100

    Generate a graph with two lines
    Create Generatos which are utility classes to generate values to be
    used on the graphs
    
    Also include a new Jasmine matcher

---
+++ app/classes/graphs-data/generators.js
@@ -0,0 +1,20 @@
+var redefine = require('redefine');
+var _        = require('lodash');
+var moment   = require('moment');
+
+var Generators = redefine.Class({
+  everyDay: function(from, to, what) {
+    var dateFrom     = moment([from.getFullYear(), from.getMonth(), from.getDate()]);
+    var dateTo       = moment([to.getFullYear(), to.getMonth(), to.getDate()]);
+    var numberOfDays = dateTo.diff(dateFrom, 'days');
+
+    return _.times(numberOfDays, function(n){
+      // Have to create a new date on each round or dateFrom
+      // will accumulate all aditions
+      return {date: moment(dateFrom).add(n + 1, 'days').toDate(), value: what};
+    });
+  },
+
+});
+
+module.exports = Generators;
--- app/components/graphs.react.js
+++ app/components/graphs.react.js
@@ -3,7 +3,8 @@
 var React           = require('react');
 var Fluxxor         = require('fluxxor');
 var _               = require('lodash');
-var Transformations = require('../classes/graphs-data-transformations/transformations');
+var Transformations = require('../classes/graphs-data/transformations');
+var Generators      = require('../classes/graphs-data/generators');
 
 var StoreWatchMixin = Fluxxor.StoreWatchMixin;
 var FluxxorMixin    = Fluxxor.FluxMixin(React);
@@ -18,11 +19,18 @@ var Graphs = React.createClass({
   },
 
   render: function(attribute) {
-    var data = new Transformations().accumulatedPerDay(this.transform_data());
+    var data_transformed = this.transform_data(this.state.entries.models);
+    var data = new Transformations().accumulatedPerDay(data_transformed.expenses);
+
+
+    var dataNovember     = this.transform_data(this.state.entries.findByMonth(10));
+    var expensesNovember = new Transformations().accumulatedPerDay(dataNovember.expenses);
+
 
 
     // Important check. If the array is empty
-    // the Graph library will crash
+    // the Graph library will crash. By graph library
+    // I mean the data_graphic function
     if (data.length > 0) {
       data_graphic({
         title: "Annotations",
@@ -34,18 +42,43 @@ var Graphs = React.createClass({
         x_accessor: 'date',
         y_accessor: 'value'
       });
-    }
 
-    return <div className="r-graph-container" id="target"></div>;
+  }
+
+  if (expensesNovember.length > 0)
+      {
+        data_graphic({
+          data: [expensesNovember, [{date: expensesNovember[0].date, value:2200}, {date: expensesNovember[expensesNovember.length - 1].date, value: 2200}]],
+          width: 1000,
+          interpolate: 'linear',
+          target: '#target2',
+          x_accessor: 'date',
+          y_accessor: 'value'
+        });
+      }
+
+
+    return <div>
+      <div className="r-graph-container" id="target"></div>
+      <div className="r-graph-container" id="target2"></div>
+    </div>;
   },
 
-  transform_data: function() {
+  transform_data: function(entries) {
     if (!this.state.entries) return;
 
-    return this.state.entries.map(function(e){
-      return {date: e.get('created_at'), value: Math.abs(e.get('amount'))};
+    d = {expenses: [], incomes: []};
+
+    entries.map(function(e){
+      if (e.get('amount') < 0)
+        d.expenses.push( {date: e.get('created_at'), value: Math.abs(e.get('amount'))} );
+      else
+        d.incomes.push( {date: e.get('created_at'), value: e.get('amount')} );
     });
+
+    return d;
   }
+
 });
 
 module.exports = Graphs;
