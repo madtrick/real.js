14ca4d63eeed3ada3f59b28ba10163994435b1bc
Farruco Sanjurjo <madtrick@gmail.com>
2014-12-12 21:12:58 +0100

    Add #findByTag method

--- app/collections/accounting_entries.js
+++ app/collections/accounting_entries.js
@@ -1,6 +1,7 @@
 var moment          = require('moment');
 var Backbone        = require('backbone-associations');
 var Cocktail        = require('backbone.cocktail');
+var _               = require('lodash');
 var Stats           = require('./mixins/accounting-entries/stats.js');
 var AccountingEntry = require('../models/accounting-entry');
 var config          = require('../../config');
@@ -17,6 +18,15 @@ var AccountingEntries = Backbone.Collection.extend({
     return this.filter(function(accountingEntry){
       return accountingEntry.get('created_at').getMonth() == month;
     });
+  },
+
+  findByTags: function(tags) {
+    return this.filter(function(accountingEntry){
+      return _.any(accountingEntry.get('tags'), function(tag){
+        return tags.indexOf(tag) != -1;
+      });
+    });
+  },
   }
 });
 module.exports = AccountingEntries;
