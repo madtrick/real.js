6c1b5c34eba12d181102da06c24fafde6fe20ab9
Farruco Sanjurjo <madtrick@gmail.com>
2015-02-01 18:41:22 +0100

    Fix issue with date and time zones

--- app/models/accounting-entry.js
+++ app/models/accounting-entry.js
@@ -1,4 +1,5 @@
 var Backbone = require('backbone-associations');
+var _        = require('lodash');
 var moment   = require("moment");
 var User     = require('./user');
 var config   = require("../../config");
@@ -18,6 +19,22 @@ var AccountingEntry = Backbone.AssociatedModel.extend({
     date: function() {
       return moment(this.attributes.date).toDate();
     }
+  },
+
+  toJSON: function() {
+    /*
+     * Date.prototype.toJSON converts date objects without taking
+     * the timezone into account substracting one day in some cases
+     *
+     * See this question in SO for more info:
+     *
+     * http://stackoverflow.com/questions/11382606/javascript-date-tojson-dont-get-the-timezone-offset
+     */
+
+    var attributes  = _.cloneDeep(this.attributes);
+    attributes.date = attributes.date; //return the date without the mutator transformation
+
+    return attributes;
   }
 });
 module.exports = AccountingEntry;
